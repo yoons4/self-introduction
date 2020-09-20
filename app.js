3d1752f0f03edff7069eb57000727e2e181eacca
Sangha Yoon <yoons4@spu.edu>
2020-09-20 00:02:51 -0700

    Changed app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
 			response.statusCode = 200;
 			response.setHeader('Content-Type', 'text/plain');
-			response.end('Hello World\n\nCool!');
+			response.end('Welcome to my Web Application\n\nMy name is: Sangha Yoon');
 });
 
 server.listen(port, (err) => {
